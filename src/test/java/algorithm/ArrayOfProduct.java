c23a0a1b99be0c38407568579f6dacd332c8972a
Dhamotharan <prithivirajan@gmail.com>
2017-10-05 19:23:28 +0530

    algorithm for product of array

---
+++ src/test/java/algorithm/ArrayOfProduct.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+public class ArrayOfProduct {
+
+	public int[] getProduct(int[] inputs) {
+		
+		if(inputs == null || inputs.length == 0 || inputs.length == 1)
+			return new int[0];
+		
+		int product=1, N=inputs.length;
+		int[] productArray = new int[N];
+		for(int i=0;i<N;i++){
+			productArray[i]=product;
+			product *= inputs[i];
+		}
+		
+		product=1;
+		int[] outArray = new int[N];
+		for(int i=N-1; i>=0; i--){
+			outArray[i]=productArray[i]*product;
+			product *= inputs[i];
+		}
+		return outArray;
+	}
+
+}
---
+++ src/test/java/algorithm/TestArrayOfProducts.java
@@ -0,0 +1,41 @@
+package algorithm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestArrayOfProducts {
+	
+	private ArrayOfProduct array;
+	
+	@Before
+	public void setup(){
+		array = new ArrayOfProduct();
+	}
+
+	@Test
+	public void arrayOfOneIntegerProduct(){
+		int[] outArray = array.getProduct(new int[]{10});		
+		assertEquals("array should be empty", 0, outArray.length);
+	}
+	
+	@Test
+	public void arrayOfTwoIntegerProduct(){
+		int[] outArray = array.getProduct(new int[]{10, 20});		
+		assertArrayEquals(new int[]{20, 10}, outArray);
+	}
+	
+	@Test
+	public void testArrayOfThreeIntegers(){
+		int[] outArray = array.getProduct(new int[]{10, 20, 30});		
+		assertArrayEquals(new int[]{600, 300, 200}, outArray);
+	}
+	
+	@Test
+	public void testArrayOfFourIntegers(){
+		int[] outArray = array.getProduct(new int[]{10, 20, 30, 40});		
+		assertArrayEquals(new int[]{24000, 12000, 8000, 6000}, outArray);
+	}
+
+}
